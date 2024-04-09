7aa063a787b47671d6bfe6747880235216021acb
Nqnt41 <covenyr@ufl.edu>
2024-04-09 14:14:24 -0400

    Committing changes to Counter.test.js to allow for JS testing.

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,57 @@
 // import necessary react testing library helpers here
 // import the Counter component here
+import Counter from '../components/Counter'
+import {render, screen, fireEvent} from "@testing-library/react";
 
 beforeEach(() => {
-  // Render the Counter component here
+  // Render the Counter component
+    render(<Counter />);
 })
 
 test('renders counter message', () => {
-  // Complete the unit test below based on the objective in the line above
+  // Checks that the Counter text is displayed on-screen through counterMessage
+  const counterMessage = screen.getByText(/counter/i);
+  expect(counterMessage).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
-  // Complete the unit test below based on the objective in the line above
+  // Checks that count starts at zero through initialCount
+  const initialCount = screen.getByTestId("count");
+  expect(initialCount.textContent).toBe("0");
 });
 
 test('clicking + increments the count', () => {
-  // Complete the unit test below based on the objective in the line above
+  // Check that text and count properly display
+  const incrementButton = screen.getByText("+");
+  const countDisplay = screen.getByTestId("count");
+
+  // fireEvent increases count by calling incrementButton
+  fireEvent.click(incrementButton);
+
+  // Check that count actually incremented
+  expect(countDisplay.textContent).toBe("1");
+
+  // Increment count by ten, check that still works
+  for (let i = 0; i < 10; i++) {
+    fireEvent.click(incrementButton);
+  }
+  expect(countDisplay.textContent).toBe("11");
 });
 
 test('clicking - decrements the count', () => {
-  // Complete the unit test below based on the objective in the line above
+  // Check that text and count properly display
+  const decrementButton = screen.getByText("-");
+  const countDisplay = screen.getByTestId("count");
+
+  // fireEvent decreases count by calling decrementButton
+  fireEvent.click(decrementButton);
+
+  // Check that count actually decremented and can be in the negatives
+  expect(countDisplay.textContent).toBe("-1");
+
+  // Decrement count by ten, check that still works
+  for (let i = 0; i < 10; i++) {
+    fireEvent.click(decrementButton);
+  }
+  expect(countDisplay.textContent).toBe("-11");
 });
